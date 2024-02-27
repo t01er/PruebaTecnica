65a401ae64f82f5e5ab0d31a8df0a8ffa8bbe077
toler <123109464+t01er@users.noreply.github.com>
2024-02-27 14:05:40 -0500

    Update Imagen.jsx
    Cambie por la url de una imagen

--- src/assets/components/Imagen.jsx
+++ src/assets/components/Imagen.jsx
@@ -3,20 +3,22 @@ import axios from 'axios';
 
 const PixabayComponent = ({ countryName }) => {
   const [image, setImage] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchImage = async () => {
       try {
-        const apiKey = '33642408-24d7be1d992c588d808935a62'; 
+        const apiKey = '33642408-24d7be1d992c588d808935a62';
         const response = await axios.get(`https://pixabay.com/api/?key=${apiKey}&q=${countryName} landscape&image_type=photo&per_page=100`);
- 
+
         if (response.data.hits.length > 0) {
           setImage(response.data.hits[0]);
         } else {
-          // console.warn('No se encontraron imágenes para', countryName);
+          setError(true); 
         }
       } catch (error) {
         console.error('Error', error);
+        setError(true); 
       }
     };
 
@@ -24,10 +26,16 @@ const PixabayComponent = ({ countryName }) => {
   }, [countryName]);
 
   return (
-    <div>     
-      {image && (
+    <div>
+      {error ? (
+        <div>        
+          <img loading='lazy' className='w-full h-96 object-cover rounded-2xl' src="https://arqueologiadelperu.com/wp-content/uploads/2014/09/valle-del-sondongo.jpg" alt="Imagen de respaldo" />
+        </div>
+      ) : (
         <div>
-          <img loading='lazy' className='w-full h-96 object-cover rounded-2xl' src={image.webformatURL} alt={image.tags} />
+          {image && (
+            <img loading='lazy' className='w-full h-96 object-cover rounded-2xl' src={image.webformatURL} alt={image.tags} />
+          )}
         </div>
       )}
     </div>
